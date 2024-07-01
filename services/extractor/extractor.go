ee3fe2b87c8c6010ac54ae182bf433156a0822b2
needhourger <chainyuuki@gmail.com>
2024-07-01 17:23:26 +0800

    feat: remove go-fitz epub support

--- services/extractor/extractor.go
+++ services/extractor/extractor.go
@@ -9,10 +9,6 @@ func Extract(book *database.Book) error {
 	switch book.Extension {
 	case ".txt":
 		return extractTxt(book)
-	case ".epub":
-		return extractEPUB_PDF(book)
-	case ".pdf":
-		return extractEPUB_PDF(book)
 	}
 	return errors.New("unsupported file type")
 }
--- services/reader/reader.go
+++ services/reader/reader.go
@@ -9,7 +9,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/gen2brain/go-fitz"
 	"github.com/patrickmn/go-cache"
 )
 
@@ -35,27 +34,8 @@ func ReadChapter(book *database.Book, chapter *database.Chapter) (any, error) {
 	switch book.Extension {
 	case ".txt":
 		return ReadChapterTXT(book, chapter)
-	case ".epub":
-		return ReadChapterEPUB_PDF(book, chapter)
-	case ".pdf":
-		return ReadChapterEPUB_PDF(book, chapter)
 	}
 	return "", ErrFileType
-}
-
-func ReadChapterEPUB_PDF(book *database.Book, chapter *database.Chapter) (any, error) {
-	doc, found := readerCache.Get(book.Path)
-	if !found {
-		doc, err := fitz.New(book.Path)
-		if err != nil {
-			defer doc.Close()
-			return "", err
-		}
-		readerCache.Set(book.Path, doc, cache.DefaultExpiration)
-		return doc.HTML(int(chapter.Start), false)
-	}
-
-	return doc.(*fitz.Document).HTML(int(chapter.Start), false)
 }
 
 func ReadChapterTXT(book *database.Book, chapter *database.Chapter) (string, error) {
